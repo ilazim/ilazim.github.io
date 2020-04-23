51acfdb4ed6de158468893fe77a0fec1b7b37988
Inteser Lazim <inteser.lazim.cse@ulab.edu.bd>
2020-04-23 20:05:53 +0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,14 @@
+$(document).on('ready', function () {
+  $(".vertical-center-4").slick({
+    adaptiveHeight: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    
+  });
+});

175302557d3e59d62916cd1337f09de125c3a89d
Inteser Lazim <inteser.lazim.cse@ulab.edu.bd>
2020-04-29 01:38:46 +0600

    Update init.js

--- init.js
+++ init.js
@@ -1,14 +1,13 @@
-$(document).on('ready', function () {
-  $(".vertical-center-4").slick({
-    adaptiveHeight: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
-    
-  });
+$(document).on('ready', function() {
+    $(".vertical-center-4").slick({
+        adaptiveHeight: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+    });
 });
